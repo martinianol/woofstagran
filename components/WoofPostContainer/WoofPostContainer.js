b5b66ac74f3f2f5335bb3d80b489df0d02c9bf02
Mars <martinianol@outlook.com>
2021-10-08 17:18:48 -0300

    Broke app :(

--- components/WoofPostContainer/WoofPostContainer.js
+++ components/WoofPostContainer/WoofPostContainer.js
@@ -7,6 +7,10 @@ const WoofPostContainer = (props) => (
   <View style={styles.container}>
     <WoofPost url={"https://picsum.photos/128/128"} />
     <WoofPost url={"https://picsum.photos/128/128"} />
+    <WoofPost url={"https://picsum.photos/128/128"} />
+    <WoofPost url={"https://picsum.photos/128/128"} />
+    <WoofPost url={"https://picsum.photos/128/128"} />
+    <WoofPost url={"https://picsum.photos/128/128"} />
   </View>
 );
 
